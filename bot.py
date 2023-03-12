a5bc6a78340a2d3ffd0281522fc88f9e237b62ab
prr133f <dkrilov2005@gmail.com>
2023-03-12 22:05:40 +0300

    final version

--- bot.py
+++ bot.py
@@ -6,22 +6,31 @@ import api
 
 dotenv.load_dotenv()
 
-token = os.getenv('BOT_TOKEN')
-
-bot = AsyncTeleBot(token)
+bot = AsyncTeleBot(os.getenv('BOT_TOKEN'))
 
 @bot.message_handler(commands=['start'])
 async def start(msg):
-    await bot.send_message(msg.chat.id, "Привет!! Я умею переводить код с одного языка на другой используя GPT модель! Попробуй прислать мне код в формате: \nЯзык на котором написан:Язык на который перевести\nКод")
+    await bot.send_message(msg.chat.id, "Привет!! Я умею переводить код с одного языка на другой используя GPT модель! Попробуй прислать мне код в формате: \n/translate\nЯзык на котором написан:Язык на который перевести\nКод")
 
 
-@bot.message_handler(func=lambda message: True)
+@bot.message_handler(commands=["translate"])
 async def translate(msg):
-    lang1, lang2 = msg.text.split(':')[0], msg.text.split(':')[1].split('\n')[0]
-    src = ''.join(msg.text.split(':')[1].split('\n')[1:])
+    text = ''.join(msg.text.split('\n')[1:])
+    if text == '':
+        await bot.send_message(msg.chat.id, "Вы забыли сказать мне что переводить)")
+    try:
+        lang1, lang2 = text.split(':')[0], text.split(':')[1].split('\n')[0]
+        src = ''.join(text.split(':')[1].split('\n')[1:])
+    except:
+        await bot.send_message(msg.chat.id, "Неверный формат данных. Воспользуйтесь /start")
+        return
+    
     g = api.GPT(os.getenv('TOKEN'))
-
     await bot.send_message(msg.chat.id, g.translate(src, lang1, lang2)['choices'][0]['message']['content'])
+
+@bot.message_handler(func=lambda msg: True)
+async def random_message(msg):
+    await bot.send_message(msg.chat.id, "Пришли мне код в приведенном выше формате. Я могу повторить его по команде /start")
     
 
 asyncio.run(bot.polling())
